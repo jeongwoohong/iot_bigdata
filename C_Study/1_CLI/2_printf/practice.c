574907351ed2a0d329d326a7ae26da873a4cf3ce
jeongwoo hong <you@example.com>
2019-05-02 09:27:40 +0900

    5월 1일 수업내용

---
+++ C_Study/1_CLI/2_printf/practice.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+	printf("현재온도 : %d도,  오존지수 : %f, 정확도 : %d%%\n", 22, 0.075, 99);
+	printf("순위\t      영화명\t\t변동폭\n");
+	printf("  1\t 어벤져스엔드게임\t  -0\n");
+	printf("  2\t     캡틴마블\t\t  -0\n");
+	printf("  3\t       생일\t\t  -0\n");
+	printf("  4\t      미성년\t\t  -0\n");
+}
