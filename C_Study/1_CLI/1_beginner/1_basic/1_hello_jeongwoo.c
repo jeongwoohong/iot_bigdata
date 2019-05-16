3eef0fa324d34936017ec312ecf2037fe4ab12fe
jeongwoo hong <you@example.com>
2019-05-16 09:35:52 +0900

    rename

--- C_Study/1_CLI/1_beginner/1_basic/1_hello_jeongwoo.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main(){
-	printf("Hello_Jeongwoo!\n");
-	printf("Hello_Jeongwoo!");
-}
--- C_Study/1_CLI/1_beginner/1_basic/2_hello_jeongwoo2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main(){
-	printf("Hello_Jeongwoo!");
-	printf("Hello_Jeongwoo!");
-}
