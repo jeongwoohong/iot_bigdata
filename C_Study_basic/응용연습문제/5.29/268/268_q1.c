829c56a2f2673311cae063542c915f00404ac745
jeongwoo hong <you@example.com>
2019-06-11 17:19:28 +0900

    6/11

--- C_Study_basic/응용연습문제/5.29/268/268_q1.c
+++ C_Study_basic/응용연습문제/5.29/268/268_q1.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #pragma warning (disable:4996)
 
+
 void main() {
 	char input_str[100];
-	int i;
+	int length=0;
 
 	printf("영단어를 입력하세요 : ");
-	for (i = 0; i < 100; i++) {
-		scanf("%c", input_str[i]);
-	}
-	for (i = 0; i < 100; i++) {
-		printf("%c",input_str[i]);
-	}
+	scanf("%s", input_str);
+
+	while (input_str[length] != 0)
+		length++;
+
+	printf("입력한 영단어의 길이 : %d", length);
 
 }
